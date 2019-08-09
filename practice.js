6245d5e3cc89c6dcdd006148f4915be4aa3e82e1
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-08-09 04:17:38 -0500

    first commit

--- practice.js
+++ practice.js
@@ -3,30 +3,34 @@
 //Create a variable called myName that is a string data type
 
 //Code here
+var myName= "Maegan"
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
 //Code here
+var myAge= 25
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
 //Code here
+var lovesCode= "True"
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
-
+var greatestFear= undefined
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
+var devMountainGoal= null 
 
 //////////////////PROBLEM 6////////////////////
 
@@ -36,6 +40,12 @@
 //plus the value of the name parameter.
 
 //Code here
+function greeting(name){
+  return "Hello, " + name;
+}
+
+
+
 
 //////////////////PROBLEM 7////////////////////
 
@@ -43,6 +53,12 @@
 //Name it newGreeting.
 
 //Code Here
+var newGreeting=function(name){
+ return "Hello, " + name;
+}
+
+
+
 
 //////////////////PROBLEM 8////////////////////
 
@@ -50,6 +66,7 @@
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
+var groceries=["apples","milk","eggs","bread"]
 
 //////////////////PROBLEM 9////////////////////
 
@@ -59,11 +76,19 @@
 //and goodBoy (a boolean).
 
 //Code Here
+var dog = 
+ name= "Rowdy",
+ color= "brown",
+ age= 8,
+ goodBoy= true
+ ;
+
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
+var devMountainClassPet = dog.name
 
 //////////////////PROBLEM 10////////////////////
 
@@ -75,6 +100,16 @@
 // with NAMEPARAM being the name parameter being passed in
 
 // Code here
+function nameCheck(name) {
+	if (name = "Steven") {
+		return "What is up Steven?"
+	} else if (name = "Bryan") {
+		return "Hey Bryan!"
+	} else (nameCheck="NAMEPARAM") {
+		return "Cool name, NAMEPARAM"
+	}
+}
+
 
 //////////////////PROBLEM 11////////////////////
 
@@ -83,11 +118,17 @@
 // The add function should return the two parameters added together
 
 //Code Here
+function add(num1+num2)
+
+
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
 //Code Here
+function add(num1+num2)
+add(3+4)
+mathSum=
 
 //////////////////PROBLEM 12////////////////////
 
